9756bc0b9c00ea690bc4e5802cc94882ec2e4688
Codemonk-adi <56336914+Codemonk-adi@users.noreply.github.com>
2021-03-03 14:07:28 +0530

    working soln

---
+++ grouping_children.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    vector<float> ages;
+    int n;
+    cin>>n;
+    float t;
+    for(int i =0; i<n;i++){
+        cin>>t;
+        ages.push_back(t);
+    }
+    sort(ages.begin(),ages.end());
+    int p=1;
+    float min=ages[0];
+    for( auto i : ages){
+        if(i-min>1){
+            min=i;
+            p++;
+        }
+    }
+    cout<<p;
+    return 0;
+}
